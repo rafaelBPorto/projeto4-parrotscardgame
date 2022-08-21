2bfc8c59c17ac6ebb1151a466bca2ecd2e93d045
Rafael Braga Porto <rafaelbporto88@gmail.com>
2022-08-20 22:16:00 -0300

    pergunta quantas cartas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 
-function virarCarta(cartaSelecionada){
+//Perguntar número de cartas para jogador
+let numeroDeCartas = prompt("Bem Vindo ao Parrot Card Game!!!\nCom quantas cartar quer jogar");
 
-    cartaSelecionada.classList.toggle("virar")
-
+while ((numeroDeCartas%2 !==0) || numeroDeCartas > 14 ||numeroDeCartas < 2){
+    numeroDeCartas = prompt("Você precisa escolher um número par de 2 a 14.\n Com quatas cartas quer jogar");
 }
+
+//Distribuir as cartas no navegador
+/*
+for 
+*/
+//Virar Carta
+function virarCarta(cartaSelecionada){
+    cartaSelecionada.classList.toggle("virar");
+}
+
