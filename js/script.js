62865cc9a78278959454db4e13dcd4b94eca740b
Rafael Braga Porto <rafaelbporto88@gmail.com>
2022-08-20 22:33:20 -0300

    distribuir cartas

--- js/script.js
+++ js/script.js
@@ -5,13 +5,23 @@ let numeroDeCartas = prompt("Bem Vindo ao Parrot Card Game!!!\nCom quantas carta
 while ((numeroDeCartas%2 !==0) || numeroDeCartas > 14 ||numeroDeCartas < 2){
     numeroDeCartas = prompt("Você precisa escolher um número par de 2 a 14.\n Com quatas cartas quer jogar");
 }
-
 //Distribuir as cartas no navegador
-/*
-for 
-*/
-//Virar Carta
-function virarCarta(cartaSelecionada){
-    cartaSelecionada.classList.toggle("virar");
+const ul = document.querySelector("ul");
+for (let i = 0; i < numeroDeCartas; i++) {
+    ul.innerHTML += `
+    <li class="carta" onclick="virarCarta(this)">
+        <div class="tamanhoCarta verso">
+            <img src="./img/front.png" alt="parrot">
+        </div>
+        <div class="tamanhoCarta frente">
+            <img src="./img/bobrossparrot.gif" alt="bobrossparrot">
+        </div>
+    </li>
+    `;
 }
 
+
+//Virar Carta
+function virarCarta(cartaSelecionada) {
+    cartaSelecionada.classList.toggle("virar");
+}
